a6fef22b6ec9de3d617930150b455a35462d1b4d
msbrandt00 <msbrandt00@gmail.com>
2023-11-04 22:32:59 -0600

    Create toggl request wrapper, initial routes

---
+++ src/lib/server/toggl-client.ts
@@ -0,0 +1,19 @@
+import { TOGGL_API_TOKEN } from "$env/static/private";
+export async function togglRequest(endpoint) {
+  const url = `https://api.track.toggl.com/api/v9${endpoint}`;
+
+  const headers = new Headers({
+    Authorization: `Basic ${btoa(`${TOGGL_API_TOKEN}:api_token`)}`,
+  });
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: headers,
+  });
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status ${response.status}`);
+  }
+
+  return await response.json();
+}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	export let data;
+</script>
+
+<h1>Currently logged in as {data.userData.fullname}</h1>
